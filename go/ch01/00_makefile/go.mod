af71d80c53991e7cb8693ff3718f07d5eba35930
daeikkim <12377dl@gmail.com>
2023-10-20 11:50:10 +0900

    setting

---
+++ go/ch01/00_makefile/go.mod
@@ -0,0 +1,3 @@
+module github.com/dik654/Blockchain_from_scratch/go/ch01/00_makefile
+
+go 1.19
---
+++ go/ch01/00_makefile/main.go
@@ -0,0 +1,11 @@
+// go mod init github.com/dik654/Blockchain_from_scratch/go/ch01/00_makefile
+// make run
+// make test 모두 완료
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world")
+}
