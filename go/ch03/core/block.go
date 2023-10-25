88c8bb9ae761c9cc17f7a1ea49863633501c2e17
daeikkim <12377dl@gmail.com>
2023-10-25 13:56:56 +0900

    keypair & signature

--- go/ch03/core/block.go
+++ go/ch03/core/block.go
@@ -6,7 +6,7 @@ import (
 	"encoding/binary"
 	"io"
 
-	"github.com/dik654/Blockchain_from_scratch/go/ch02/types"
+	"github.com/dik654/Blockchain_from_scratch/go/ch03/types"
 )
 
 type Header struct {
--- go/ch03/types/address.go
+++ go/ch03/types/address.go
@@ -1,3 +1,40 @@
 package types
 
+import (
+	"encoding/hex"
+	"fmt"
+)
+
 type Address [20]uint8
+
+func (a Address) ToSlice() []byte {
+	// uint8 배열을 bytes배열로 변환
+	b := make([]byte, 20)
+	for i := 0; i < 20; i++ {
+		b[i] = a[i]
+	}
+	return b
+}
+
+// fmt.Println, Printf시 자동으로 실행
+// bytes를 string으로 변환
+func (a Address) String() string {
+	return hex.EncodeToString(a.ToSlice())
+}
+
+func AddressFromBytes(b []byte) Address {
+	// bytes 배열이 20보다 작으면 패닉
+	if len(b) != 20 {
+		msg := fmt.Sprintf("given bytes with length %d should be 20", len(b))
+		panic(msg)
+	}
+
+	// bytes 배열을 uint8 배열로 변환
+	var value [20]uint8
+	for i := 0; i < 20; i++ {
+		value[i] = b[i]
+	}
+
+	// 후 타입에 맞춰 선언하여 리턴
+	return Address(value)
+}
